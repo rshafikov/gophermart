ede5260511e5fc0e6931ba9bc0a2f7d5d2c82926
Ramil Shafikov <rshafikov.work@yandex.ru>
2025-04-22 20:44:55 +0200

    feat: add config and base flags
    (cherry picked from commit be5bcd6b128ac68eb7969437be36961cf8350635)

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"github.com/rshafikov/gophermart/internal/app"
+	"log"
+)
+
+func main() {
+	app.InitConfig()
+
+	Application := app.NewApplication(app.Config)
+	err := Application.ConnectToDatabase(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
