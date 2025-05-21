9c3513d5a353e4c05d07b0ef8215b1da6ab53002
Ramil Shafikov <rshafikov.work@yandex.ru>
2025-05-21 16:50:17 +0200

    refactor: change an authentication format, small fixes.

--- internal/core/logger/logger.go
+++ internal/core/logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"strings"
 )
 
 var L = zap.NewNop()
@@ -13,12 +14,16 @@ func Initialize(level string) error {
 		return err
 	}
 	cfg := zap.NewProductionConfig()
+	if strings.ToLower(level) == "debug" {
+		cfg = zap.NewDevelopmentConfig()
+	}
 	cfg.Encoding = "console"
 	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	cfg.EncoderConfig.EncodeCaller = zapcore.ShortCallerEncoder // Optional: cleaner caller info
 	cfg.EncoderConfig.StacktraceKey = ""
 	cfg.Level = lvl
-	cfg.OutputPaths = []string{"stdout", "./app.log"}
+	cfg.OutputPaths = []string{"stdout"}
 	zl, err := cfg.Build()
 	if err != nil {
 		return err
